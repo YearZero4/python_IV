9f63b9a98195b72cae320c310a764b11b809526e
PGX <snakekillah217@gmail.com>
2024-11-25 17:22:14 -0400

    Add files via upload

---
+++ numero_dentro_del_array.py
@@ -0,0 +1,9 @@
+### BUSCAR NUMERO EN EL ARRAY ###
+array = [2, 4, 89, 20, 25, 14, 30, 60]
+numero = 14
+if numero in array:
+ print("El numero " + str(numero) + " Esta en el array")
+else:
+ print("El numero " + str(numero) + " No esta en el array")
+ 
+input("\nPresione Enter para continuar...")
